d172278c8558ae95996c38dfc2781a164240fe36
Stefan Kleineberg <stefan.kleineberg@gmx.de>
2020-04-23 00:51:08 +0200

    Der Vergelich der Zeitstempel klappt noch nicht.

--- opHoursCounter.sh
+++ opHoursCounter.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 opHoursCounterFile="opHoursCounter.txt"
-currentDateTime=$(date +%Y%m%d%H%M%S)
-callDifferenceMinutes=5
-differenceToleranceMinutes=1
-((totalDiffTimeMinutes=callDifferenceMinutes+differenceToleranceMinutes))
+currentDateTime=$(date +"%Y-%m-%d %H:%M:%S")
+echo "currentDateTime:$currentDateTime"
+callDifferenceMinutes=60
+differenceToleranceMinutes=5
+totalDifferenceMinutes=$((callDifferenceMinutes+differenceToleranceMinutes))
 if [ -f "$opHoursCounterFile" ]
 then
 	echo "$opHoursCounterFile found."
 	while read FILE_LINE
 	do
-		IFS=':' read -r -a SPLIT_ARRAY <<< "$FILE_LINE"
+		IFS='=' read -r -a SPLIT_ARRAY <<< "$FILE_LINE"
 		if [[ ${SPLIT_ARRAY[0]}  =~ ^lastAcquisition ]]
 		then
 			lastAcqDateTime=${SPLIT_ARRAY[1]}
@@ -21,20 +22,21 @@ then
 			echo "lastOperatingTimeMinutes:$lastOperatingTimeMinutes"
 		fi
 	done < $opHoursCounterFile
-	date --date="$lastAcqDateTime +$totalDiffTimeMinutes hours" +"%Y%m%d%H%M%S"
-#	if [$currentDateTime < $assumedDiffToLastAcqTime]
-#	then
-#		increaseOpHoursCounter=1
-#	else
-#		increaseOpHoursCounter=0
-#	fi
-#	echo "increaseOpHoursCounter:$increaseOpHoursCounter"
-#	ddiff -i '%Y%m%d%H%M%S' 20170817040001 20160312000101
-
+	assumedDiffToLastAcqTime=$(date --date="$lastAcqDateTime $totalDifferenceMinutes minutes" +"%Y-%m-%d %H:%M:%S")
+	echo "assumedDiffToLastAcqTime:$assumedDiffToLastAcqTime"
+	currentDateTimeUTS=$(date --date="currentDateTime" +"%s")
+	assumedDiffToLastAcqTimeUTS=$(date --date="assumedDiffToLastAcqTime" +"%s")
+	if [ $currentDateTimeUTS -le $assumedDiffToLastAcqTimeUTS ];
+	then
+		increaseOpHoursCounter=1
+	else
+		increaseOpHoursCounter=0
+	fi
+	echo "increaseOpHoursCounter:$increaseOpHoursCounter"
 else #else-branch of if [ -f "$opHoursCounterFile" ]
 	echo "$opHoursCounterFile not found."
-	line1="lastAcquisition:$currentDateTime"
-	line2="operatingTimeMinutes:0"
+	line1="lastAcquisition=$currentDateTime"
+	line2="operatingTimeMinutes=0"
 	/bin/cat <<EOM >$opHoursCounterFile
 $line1
 $line2
